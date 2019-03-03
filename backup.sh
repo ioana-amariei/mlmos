8f5a6429e540c57b9a90521feab08b0afd8d725d
root <root@localhost.localdomain>
2019-03-03 17:23:26 +0200

    Add implementation for backup

--- backup.sh
+++ backup.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
+# find all previous archived backups
+nrOfBackupFiles=$(ls | awk '/file-[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}-.*.tar/ { print }' | wc -l)
 
-
+if [ $nrOfBackupFiles -gt 3 ]; then
+	oldestBackup=$(ls -tr | awk '/file-[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}-.*.tar/ { print }' | head -1
+)
+	rm $oldestBackup
+fi	
 
 date=$(date +"%Y-%m-%u-%H-%M-%S")
 fileName=` echo $1 | cut -d. -f1`
@@ -12,3 +18,4 @@ echo $fileName
 echo $fullName
 
 tar -cf ${fullName} $1
+
