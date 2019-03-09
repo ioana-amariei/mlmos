25776fc4fd9a2978a2ca7687a55802b565544bf2
ioanabirsan <ioana.c.birsan@gmail.com>
2019-03-09 18:40:55 +0200

    Update implementation

--- homework1/bootstrap.sh
+++ homework1/bootstrap.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-echo "Hello World!" >> /var/log/system-bootstrap.log
- 
+logs=/var/log/system-bootstrap.log
+
+echo "Hello World!" >> $logs
+
+file=/etc/selinux/config
+selinux=$(grep "^SELINUX=" $file | cut -d'=' -f2) 
+
+if [[ "$selinux" == "disabled" ]]; then
+	echo "SELINUX is disabled; running setenforce 0" >> $logs
+	setenforce 0
+else
+	echo "SELINXU is not disbled; current value: $selinux"
+fi
 
